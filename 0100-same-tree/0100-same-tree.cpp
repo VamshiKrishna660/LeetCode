3f7440415b0b0d5d939029f84e3780d197646b02
Vamshi Krishna <146640385+VamshiKrishna660@users.noreply.github.com>
2025-07-10 08:50:57 +0530

    Time: 0 ms (100%), Space: 12.9 MB (14.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -10,40 +10,20 @@
  * };
  */
 class Solution {
-private:
-    int fun(TreeNode* p, TreeNode* q){
-        if(!p && !q)
-            return 0;
-        if((!p && q) || (p && !q)){
-            return -1;
-        }
-
-        if(p->val != q->val){
-            return -1;
-        }
-
-        int l = fun(p->left,q->left);
-        if(l==-1)   
-            return -1;
-        int r = fun(p->right,q->right);
-        if(r==-1)
-            return -1;
-       
-        return 0;
-    }
 public:
+    
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if((!p && q) || (p && !q)){
-            return false;
-        }
         if(!p && !q){
             return true;
         }
-
-        int x = fun(p,q);
-        if(x==-1){
+        if(!p || !q){
             return false;
         }
-        return true;
+        if(p->val != q->val){
+            return false;
+        }
+        bool res = isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return res;
+        
     }
 };
